486d7239cdaf51fe495a7544d80417023b96d96c
Alex Manolache <manolachealexdragos@gmail.com>
2023-03-29 18:06:48 +0200

    Update custom conda installer files

--- googlecolab-installer/pip-dependencies.sh
+++ googlecolab-installer/pip-dependencies.sh
@@ -4,4 +4,4 @@ conda activate $PREFIX            # do not edit
 
 # add your pip packages here; bottle is just an example!
 # replace it with your dependencies
-python -m pip install bottle
+python -m pip install matplotlib==3.5.1 mapcalc==0.2.2 pytorch-lightning==1.4.9 torch-cluster==1.6.1 torch-geometric==2.0.4 torch-scatter==2.1.1 torch-sparse==0.6.13 torch-spline-conv==1.2.2 torchmetrics==0.4.0 wandb==0.14.0
