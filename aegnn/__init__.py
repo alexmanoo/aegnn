e3b17aed8d96e275bf8fb9fe5707012d8a558583
Alex Manolache <a.d.manolache@student.tudelft.nl>
2023-03-29 02:12:31 +0200

    Change dataset and log default directories

--- aegnn/__init__.py
+++ aegnn/__init__.py
@@ -13,6 +13,6 @@ except ModuleNotFoundError:
 # Consequently, when another system is used, other than the default system, the env variable
 # can simply be changed prior to importing the `aegnn` module.
 aegnn.utils.io.setup_environment({
-    "AEGNN_DATA_DIR": "/data/storage/",
-    "AEGNN_LOG_DIR": "/data/scratch/"
+    "AEGNN_DATA_DIR": "/content/aegnn/ncaltech101/",
+    "AEGNN_LOG_DIR": "/content/aegnn/logs"
 })
